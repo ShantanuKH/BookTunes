e31c75254cceff1683188581b6e5ec60d1cbc9ac
ShantanuKH <khadseshantanu02@gmail.com>
2024-06-15 13:25:12 +0530

    pageViewBuilder

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class homepage extends StatefulWidget {
-  const homepage({super.key});
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
 
   @override
-  State<homepage> createState() => _homepageState();
+  State<Homepage> createState() => _HomepageState();
 }
 
-class _homepageState extends State<homepage> {
+class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,11 +16,65 @@ class _homepageState extends State<homepage> {
         child: Scaffold(
           body: Column(
             children: [
-              
+              Container(
+                margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ImageIcon(
+                      AssetImage("assets/Images/menu.png"),
+                      size: 22,
+                      color: Colors.black,
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.search),
+                        SizedBox(width: 10),
+                        Icon(Icons.notifications),
+                        SizedBox(width: 10),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20), // Added SizedBox to give space
+              Container(
+                // margin: const EdgeInsets.only(l),
+                child: Text(
+                  "Popular Songs",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+              Container(
+                height: 180,
+                child: PageView.builder(
+                  // The PageController(viewportFraction: 0.8) in Flutter is used to control a PageView widget with a specific configuration where each page takes up a fraction of the viewport width
+                  controller: PageController(viewportFraction: 0.8),
+                  itemCount: 5,
+                  itemBuilder: (_, i) {
+                    return Container(
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: AssetImage('assets/Images/pic-8.png'),
+                          fit: BoxFit.cover, // Adjust the fit as needed
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Homepage(),
+  ));
 }
