678f2e92c318283e7075c1fc2d33bc1b6a5662f7
ShantanuKH <khadseshantanu02@gmail.com>
2024-06-15 15:30:29 +0530

    load json file to page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -9,6 +13,28 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   PageController _pageController = PageController();
+
+// To call the json file we are first initilaizing it
+
+// Imp step to take json file in our project
+  List songs = []; // List to hold songs data
+
+  void readData() async {
+    // Json is in string format, so we have to decode it
+    await DefaultAssetBundle.of(context)
+        .loadString("json/songs.json")
+        .then((s) {
+      setState(() {
+        songs = json.decode(s); // Decode JSON string into List
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    readData(); // Call readData() in initState to load data when the widget initializes
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,19 +72,22 @@ class _HomepageState extends State<Homepage> {
                 style: TextStyle(fontSize: 30),
               ),
             ),
+            SizedBox(
+              height: 20,
+            ),
             Container(
               child: Container(
                 height: 180,
                 child: PageView.builder(
                   controller: _pageController,
-                  itemCount: 5,
-                  itemBuilder: (context, index) {
+                  itemCount: songs == null ? 0 : songs.length,
+                  itemBuilder: (_, i) {
                     return Container(
                       margin: const EdgeInsets.symmetric(horizontal: 10),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
                         image: DecorationImage(
-                          image: AssetImage('assets/Images/pic-8.png'),
+                          image: AssetImage(songs[i]["img"]),
                           fit: BoxFit.cover,
                         ),
                       ),
