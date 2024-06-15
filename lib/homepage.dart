e3e09258c80f552b0e2be63ec7a677d80061fcce
ShantanuKH <khadseshantanu02@gmail.com>
2024-06-15 12:17:24 +0530

    Initialize project

---
+++ lib/homepage.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatefulWidget {
+  const homepage({super.key});
+
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
