b1862c4700c277369ec839e080d7c24f68667b58
ShantanuKH <khadseshantanu02@gmail.com>
2024-06-15 14:08:57 +0530

    fit to the screen

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,65 +8,66 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  PageController _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: SafeArea(
-        child: Scaffold(
-          body: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ImageIcon(
-                      AssetImage("assets/Images/menu.png"),
-                      size: 22,
-                      color: Colors.black,
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.search),
-                        SizedBox(width: 10),
-                        Icon(Icons.notifications),
-                        SizedBox(width: 10),
-                      ],
-                    ),
-                  ],
-                ),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ImageIcon(
+                    AssetImage("assets/Images/menu.png"),
+                    size: 22,
+                    color: Colors.black,
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.search),
+                      SizedBox(width: 10),
+                      Icon(Icons.notifications),
+                      SizedBox(width: 10),
+                    ],
+                  ),
+                ],
               ),
-              SizedBox(height: 20), // Added SizedBox to give space
-              Container(
-                // margin: const EdgeInsets.only(l),
-                child: Text(
-                  "Popular Songs",
-                  style: TextStyle(fontSize: 30),
-                ),
+            ),
+            SizedBox(height: 20), // Added SizedBox for spacing
+            Container(
+              margin: const EdgeInsets.only(left: 20),
+              child: Text(
+                "Popular Songs",
+                style: TextStyle(fontSize: 30),
               ),
-              Container(
+            ),
+            Container(
+              child: Container(
                 height: 180,
                 child: PageView.builder(
-                  // The PageController(viewportFraction: 0.8) in Flutter is used to control a PageView widget with a specific configuration where each page takes up a fraction of the viewport width
-                  controller: PageController(viewportFraction: 0.8),
+                  controller: _pageController,
                   itemCount: 5,
-                  itemBuilder: (_, i) {
+                  itemBuilder: (context, index) {
                     return Container(
-                      margin: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
                         image: DecorationImage(
                           image: AssetImage('assets/Images/pic-8.png'),
-                          fit: BoxFit.cover, // Adjust the fit as needed
+                          fit: BoxFit.cover,
                         ),
                       ),
                     );
                   },
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
