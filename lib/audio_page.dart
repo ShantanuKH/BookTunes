557b400d96816af30c797db8a91dd5d0f8c1e587
ShantanuKH <khadseshantanu02@gmail.com>
2024-06-16 20:19:57 +0530

    working on audio file

--- lib/audio_page.dart
+++ lib/audio_page.dart
@@ -1,119 +1,64 @@
-import 'package:audioplayers/audioplayers.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_ebook_app/audiofile.dart'; // This should now import 'MyAudiofile'
+import 'package:audioplayers/audioplayers.dart' as audioplayers; // Import 'audioplayers' with a prefix
 
 class AudioPage extends StatefulWidget {
-  const AudioPage({super.key});
+  const AudioPage({Key? key}) : super(key: key);
 
   @override
   State<AudioPage> createState() => _AudioPageState();
 }
 
 class _AudioPageState extends State<AudioPage> {
-// To init audiofile
-// This late will tell compiler that the constrictor is initialiazation will be done later
-  late AudioPlayer advancedPlayer;
+  late audioplayers.AudioPlayer advancedPlayer; // Use the 'audioplayers' prefix here
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    advancedPlayer = AudioPlayer();
+    advancedPlayer = audioplayers.AudioPlayer(); // Use the 'audioplayers' prefix here
   }
 
   @override
   Widget build(BuildContext context) {
-    // We took this as based on different mobile screen/different devices we will get the same position
-    // We are etaking device height/width using mediaquery
     final double screenheight = MediaQuery.of(context).size.height;
     final double screenwidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: const Color(0xFfdee7fa),
+      backgroundColor: const Color(0xFFF2F3F8),
       body: Stack(
         children: [
+          // Your existing widget tree
           Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              height: screenheight / 3,
-              child: Container(color: Colors.blue.shade500)),
-          // AppBar Sathi
-          Positioned(
-            top: 0,
             left: 0,
             right: 0,
-            child: AppBar(
-                leading: IconButton(
-                  icon: Icon(Icons.arrow_back_ios),
-                  onPressed: () {},
-                ),
-                actions: [
-                  IconButton(
-                    icon: Icon(Icons.search),
-                    onPressed: () {},
+            top: screenheight * 0.2,
+            height: screenheight * 0.38,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: screenheight * 0.1),
+                  Text(
+                    "The Water Cure",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontFamily: "Avenir",
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "Welcome to SK",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Audiofile( // Use MyAudiofile instead of Audiofile
+                    advancedPlayer: advancedPlayer,
                   ),
                 ],
-                // backgroundColor: Colors.blue, ---> We can chnage the backgrounf color like this too but we will make it transperent
-
-                backgroundColor: Colors.transparent,
-                elevation:
-                    0.0 // By default there is elevation in the appbar and so, though we give background transperent we will see light grey coloe and to fix that we use elevation
-                ),
+              ),
+            ),
           ),
-          Positioned(
-              left: 0,
-              right: 0,
-              top: screenheight * 0.2,
-              height: screenheight * 0.38,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  color: Colors.white,
-                ),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: screenheight * 0.1,
-                    ),
-                    Text(
-                      "The Water Cure",
-                      style: TextStyle(
-                          fontSize: 30,
-                          fontFamily: "Avenir",
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "Welcome to SK",
-                      style: TextStyle(fontSize: 20),
-                    )
-                  ],
-                ),
-              )),
-
-          Positioned(
-              // For the small box in the middle
-              top: screenheight * 0.12,
-              // 150 is the width of the box that we want
-              left: (screenwidth - 150) / 2,
-              right: (screenwidth - 150) / 2,
-              height: screenheight * 0.16,
-              child: Container(
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 227, 227, 227),
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(color: Colors.white, width: 2)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(color: Colors.white, width: 5),
-                          image: DecorationImage(
-                              image: AssetImage("assets/Images/pic-1.png"),
-                              fit: BoxFit.cover)),
-                    ),
-                  )))
         ],
       ),
     );
--- lib/audiofile.dart
+++ lib/audiofile.dart
@@ -1,16 +1,121 @@
-import 'package:flutter/cupertino.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class Audiofile extends StatefulWidget {
-  const Audiofile({super.key});
+  final AudioPlayer advancedPlayer;
+
+  const Audiofile({Key? key, required this.advancedPlayer}) : super(key: key);
 
   @override
   State<Audiofile> createState() => _AudiofileState();
 }
 
 class _AudiofileState extends State<Audiofile> {
+  Duration _duration = Duration();
+  Duration _position = Duration();
+  String path = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3";
+  bool isPlaying = false;
+  List<IconData> _icons = [
+    Icons.play_circle_fill,
+    Icons.pause_circle_filled,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    
+    widget.advancedPlayer.onDurationChanged.listen((Duration d) {
+      setState(() {
+        _duration = d;
+      });
+    });
+
+    widget.advancedPlayer.onAudioPositionChanged.listen((Duration p) {
+      setState(() {
+        _position = p;
+      });
+    });
+
+    widget.advancedPlayer.setUrl(path);
+  }
+
+  Widget btnStart() {
+    return IconButton(
+      padding: const EdgeInsets.only(bottom: 10),
+      onPressed: () {
+        if (!isPlaying) {
+          widget.advancedPlayer.play(path);
+          setState(() {
+            isPlaying = true;
+          });
+        } else {
+          widget.advancedPlayer.pause();
+          setState(() {
+            isPlaying = false;
+          });
+        }
+      },
+      icon: isPlaying
+          ? Icon(_icons[1], size: 50, color: Colors.blue)
+          : Icon(_icons[0], size: 50, color: Colors.blue),
+    );
+  }
+
+  Widget loadAsset() {
+    return Container(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          btnStart(),
+        ],
+      ),
+    );
+  }
+
+  Widget slider() {
+    return Slider(
+      activeColor: Colors.red,
+      inactiveColor: Colors.grey,
+      value: _position.inSeconds.toDouble(),
+      min: 0.0,
+      max: _duration.inSeconds.toDouble(),
+      onChanged: (double value) {
+        changeToSeconds(value.toInt());
+      },
+    );
+  }
+
+  void changeToSeconds(int second) {
+    Duration newDuration = Duration(seconds: second);
+    widget.advancedPlayer.seek(newDuration);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Container(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "${_position.inMinutes}:${(_position.inSeconds % 60).toString().padLeft(2, '0')}",
+                  style: TextStyle(fontSize: 16),
+                ),
+                Text(
+                  "${_duration.inMinutes}:${(_duration.inSeconds % 60).toString().padLeft(2, '0')}",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+          loadAsset(),
+          slider(),
+        ],
+      ),
+    );
   }
 }
